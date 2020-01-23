66bc78ebc28e1197e6d0e8cfb8ee50aee32e3694
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-01-22 23:13:57 -0800

    Completed Year.java

--- lab1/Year.java
+++ lab1/Year.java
@@ -7,7 +7,7 @@ public class Year {
     static boolean isLeapYear(int year) {
         if (year % 400 == 0) {
             return true;
-        } else if (year % 4 == 0) & (year % 100 != 0) {
+        } else if (year % 4 == 0 && year % 100 != 0) {
             return true;
         } else {
             return false;
