4b6f7239a80e473f61db85199328c09d5a6e10df
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-02-21 23:11:10 -0800

    Dir

--- hw3/TrReader/TrReader.java
+++ hw3/TrReader/TrReader.java
@@ -30,24 +30,25 @@ public class TrReader extends Reader {
         if (len == 0) {
             return 0;
         }
-        for (int i = 0; i < cbuf.length; i += 1) {
+        for (int i = 0; i < len; i += 1) {
             int r = string.read();
             if (r != -1) {
                 char letter = (char) r;
-                if (i >= off && len > 0) {
-                    for (int a = 0; a < FROM.length(); a += 1) {
-                        if (FROM.charAt(a) == letter) {
-                            letter = TO.charAt(a);
-                            break;
-                        }
+                for (int a = 0; a < FROM.length(); a += 1) {
+                    if (FROM.charAt(a) == letter) {
+                        letter = TO.charAt(a);
+                        break;
                     }
-                    len -=1;
-                    cbuf[counter] = letter;
-                    counter += 1;
-                    result += 1;
+                len -=1;
+                cbuf[counter] = letter;
+                counter += 1;
+                result += 1;
                 }
             } else {
-                return r;
+                if (result == 0) {
+                    return r;
+                }
+                return result;
             }
         }
         return result;
