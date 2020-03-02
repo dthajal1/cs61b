48aefa65e6a2dbb65c32c6592ad00f768c693d7c
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-03-01 17:43:28 -0800

    djfs

--- lab6/enigma/PermutationTest.java
+++ lab6/enigma/PermutationTest.java
@@ -119,6 +119,7 @@ public abstract class PermutationTest {
         assertEquals(5, per.invert(6));
         assertEquals(4, per.invert(1));
         assertEquals(6, per.invert(12));
+        assertEquals(4, per.invert(8));
     }
 
     @Test
