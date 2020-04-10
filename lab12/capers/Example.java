8c5a1100227fdce796b58a9aa441b0ea8370adc4
Diraj Thajali <diraj.thajali@berkeley.edu>
2020-04-10 14:22:52 -0700

    skjdfh

---
+++ lab12/capers/Example.java
@@ -0,0 +1,16 @@
+package capers;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Example {
+    public static void main(String[] args) throws IOException {
+        File exampleFile = new File("example.txt");
+//        exampleFile.createNewFile();
+//        Utils.writeContents(exampleFile, "Hello World");
+//        File exampleDir = new File("exampleDir");
+//        exampleDir.mkdir();
+//        System.out.println(Utils.readContentsAsString(exampleFile));
+
+    }
+}
---
+++ lab12/capers/Example2.java
@@ -0,0 +1,21 @@
+package capers;
+
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashSet;
+
+public class Example2 implements Serializable {
+    public static void main(String[] args) {
+        //To convert to objects to strings and save it in a file
+//        HashSet<String> set = new HashSet<>();
+//        set.add("Diraj");
+//        set.add("Rashmi");
+//        set.add("Dipika");
+        File f = new File("set");
+//        Utils.writeObject(f, set);
+        //To convert back to objects
+        HashSet<String> setLoaded = Utils.readObject(f, HashSet.class);
+        System.out.println(setLoaded);
+    }
+}
